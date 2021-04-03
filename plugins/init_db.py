543a4386b2b1b154d2a3172d57128519842754eb
zstone12 <522089185@qq.com>
2021-04-03 17:57:16 +0800

    Update init_db.py

--- plugins/init_db.py
+++ plugins/init_db.py
@@ -5,7 +5,10 @@ DBNAME = cfg["influxdb"]["dbname"]
 USER = cfg["influxdb"]["user"]
 PASSWORD = cfg["influxdb"]["password"]
 
-influx_client = InfluxDBClient(database=DBNAME,host='localhost',username=USER,password=PASSWORD)
+influx_client = InfluxDBClient(database=DBNAME,host='localhost',username=USER,password=PASSWORD
+influx_client.get_list_database()
+influx_client.create_database('lmp')
+influx_client.get_list_database()
 tmp = [{"measurement": None, "tags": {}, "fields": {}, }]
 influx_client.write_points(tmp)
 
